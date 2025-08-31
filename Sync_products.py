2fe5d48eb2cf47c3fbfcd410bf1fb6037080aa6b
BDatamon <bocampo@datamon.es>
2025-08-31 18:05:59 +0200

    last version sunday

--- Sync_products.py
+++ Sync_products.py
@@ -1,6 +1,7 @@
 import config
 import requests
 import json
+import base64
 
 def get_products_id(): 
     try:
@@ -41,11 +42,88 @@ def get_productos_details(id):
         return None
 
 
+
+
+def get_image(id_img, name):
+    try: 
+        base_url = config.base_url  # Usa tu dominio PrestaShop real
+
+        # Construir ruta con dígitos del id_img
+        id_str = str(id_img)            #Convierte todo formato a string
+        id_path = "/".join(list(id_str))#list():sirve para convertir cualquier objeto iterable (como una cadena, tupla o conjunto) en una lista
+                                        #join():sirve para unir los elementos de un iterable (como una lista o tupla) en una sola cadena de texto
+        url_img = f"{base_url}/img/p/{id_path}/{id_str}-home_default.jpg"
+        headers = {"User-Agent": "Mozilla/5.0"}
+
+        response = requests.get(url_img, headers=headers, timeout=10)
+        response.raise_for_status()
+
+        imagen_objeto = base64.b64encode(response.content).decode('utf-8')
+        if imagen_objeto:
+            print(f'✅🎉 Imagen de {name} descargada con éxito desde {url_img}')
+            return imagen_objeto
+
+    except Exception as e:
+        print(f"❌ Error al descargar imagen {id_img} de {name}: {e}")
+        return None
+
+
+
+
+def create_product_odoo(producto_odoo):
+    try:
+        product_up = config.models.execute_kw(
+                config.db, 
+                config.uid, 
+                config.password,
+                'product.template', 
+                'create',
+                [producto_odoo]
+        )
+        print(f"✅ Producto creado: {nombre} (ID: {id_product})")
+        return product_up
+    
+
+    except Exception as e:
+        print(f"❌ Error al crear {e}")
+        return None
+
+
 if __name__=="__main__":
     productos_id = get_products_id()
     if productos_id:
         for id in productos_id:
-            id_numerico = id.get('id')
-            productos_detail = get_productos_details(id_numerico)
-            if productos_detail:    
-                print(json.dumps(productos_detail, indent=2, ensure_ascii=False))    
+            id_product = id.get('id')
+            producto_detail = get_productos_details(id_product)
+            if producto_detail:
+                nombre = producto_detail.get('name')
+                price_venta = producto_detail.get('price')
+                coste = producto_detail.get('wholesale_price')
+                description = producto_detail.get('description')
+                referencia =producto_detail.get('reference')
+                #Obtener Id de la imagen que esta dentro del objeto associations
+                field_image = producto_detail.get('associations', {}).get('images')
+                id_image = field_image[0]['id']
+                imagen_producto = get_image(id_image, nombre)
+
+                #Datos Odoo
+                producto_odoo = {
+                    "name": nombre,
+                    "default_code": referencia,
+                    "list_price": price_venta,
+                    "standard_price": coste,
+                    "type": "consu",
+                    "purchase_ok": True,
+                    "sale_ok": True,
+                    "uom_id": 1,  # Unidad de medida por defecto (1 = Units)
+                    "currency_id": 125,  # EUR (según el ejemplo)
+                    "public_description": str(description),
+                    "available_in_pos": True
+                }
+                if imagen_producto:
+                        producto_odoo["image_1920"] = imagen_producto
+                #crear_productos       
+                upload_odoo = create_product_odoo(producto_odoo)
+
+
+                # print(json.dumps(producto_detail, indent=2, ensure_ascii=False))    
--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import requests
+import xmlrpc.client
 
-
-
+#Conexion PrestaShop
+base_url = "https://datamon2.ginernet.net"
 api_key = "F62Y8AR886NH9S2XL5VK6CLCYSQQQI34"
 prestashop_url ="https://datamon2.ginernet.net/api"
 
@@ -14,4 +15,20 @@ if response.status_code == 200:
 else:
     print('❌Conexion defectuosa:', response.status_code, response.text)
 
+
+
+#Conexion Odoo
+url = "https://cremascaseras.odoo.com"
+db = "cremascaseras"
+username = "bocampo@datamon.es"
+password = "7cf1dfb233d14457bff392c9ad8e9fe8e517d541"
+
+try:
+    common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common')
+    uid = common.authenticate(db, username, password, {})
+    models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
+    print(f"✅ Conexión a Odoo exitosa. Usuario ID: {uid}")
+except Exception as e:
+    print(f"❌ Error de conexión: {e}")
+    exit()
 
